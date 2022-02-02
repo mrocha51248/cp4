4fd1701925ca301c6caa1ef099d8c9421bbf72c6
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-02 14:19:05 +0100

    create UserFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public const USERS = [
+        [
+            'email' => 'admin@cp4.gg',
+            'name' => 'Administrator',
+            'roles' => ['USER_ADMIN'],
+        ],
+        [
+            'email' => 'user1@cp4.gg',
+            'name' => 'User1',
+        ],
+        [
+            'email' => 'user2@cp4.gg',
+            'name' => 'User2',
+        ],
+        [
+            'email' => 'user3@cp4.gg',
+            'name' => 'User3',
+        ],
+    ];
+
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::USERS as $userData) {
+            $user = new User();
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $userData['password'] ?? 'password');
+            $user
+                ->setEmail($userData['email'])
+                ->setName($userData['name'])
+                ->setRoles($userData['roles'] ?? [])
+                ->setPassword($hashedPassword)
+            ;
+
+            $manager->persist($user);
+            $this->addReference('user_' . strtolower($userData['name']), $user);
+        }
+
+        $manager->flush();
+    }
+}
