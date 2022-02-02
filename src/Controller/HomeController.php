e7a11e6e7cbfbe0133022883cbe7b30cc85e8477
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-02 20:16:42 +0100

    create HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\GameRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'home_')]
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository, GameRepository $gameRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'categories' => $categoryRepository->findMostRaced(5),
+            'games' => $gameRepository->findMostRaced(10),
+        ]);
+    }
+}
