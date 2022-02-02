d324be559145d2407b0c5631eb400bb89eac7e78
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-02 11:53:16 +0100

    create RaceResult entity

---
+++ src/Entity/RaceResult.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RaceResultRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RaceResultRepository::class)]
+class RaceResult
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $startedAt;
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private $finishedAt;
+
+    #[ORM\Column(type: 'integer')]
+    private $startElo;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $finishElo;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'raceResults')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartedAt(): ?\DateTimeImmutable
+    {
+        return $this->startedAt;
+    }
+
+    public function setStartedAt(\DateTimeImmutable $startedAt): self
+    {
+        $this->startedAt = $startedAt;
+
+        return $this;
+    }
+
+    public function getFinishedAt(): ?\DateTimeImmutable
+    {
+        return $this->finishedAt;
+    }
+
+    public function setFinishedAt(?\DateTimeImmutable $finishedAt): self
+    {
+        $this->finishedAt = $finishedAt;
+
+        return $this;
+    }
+
+    public function getStartElo(): ?int
+    {
+        return $this->startElo;
+    }
+
+    public function setStartElo(int $startElo): self
+    {
+        $this->startElo = $startElo;
+
+        return $this;
+    }
+
+    public function getFinishElo(): ?int
+    {
+        return $this->finishElo;
+    }
+
+    public function setFinishElo(?int $finishElo): self
+    {
+        $this->finishElo = $finishElo;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -26,6 +28,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(type: 'string', length: 255)]
     private $name;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: RaceResult::class, orphanRemoval: true)]
+    private $raceResults;
+
+    public function __construct()
+    {
+        $this->raceResults = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -105,6 +115,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|RaceResult[]
+     */
+    public function getRaceResults(): Collection
+    {
+        return $this->raceResults;
+    }
+
+    public function addRaceResult(RaceResult $raceResult): self
+    {
+        if (!$this->raceResults->contains($raceResult)) {
+            $this->raceResults[] = $raceResult;
+            $raceResult->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRaceResult(RaceResult $raceResult): self
+    {
+        if ($this->raceResults->removeElement($raceResult)) {
+            // set the owning side to null (unless already changed)
+            if ($raceResult->getUser() === $this) {
+                $raceResult->setUser(null);
+            }
+        }
 
         return $this;
     }
