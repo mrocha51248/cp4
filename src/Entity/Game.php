a7d55a6901dcf51f87858284feb8181c06e9985e
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-02 21:00:20 +0100

    add unique contraints to Category and Game entities

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -6,8 +6,11 @@ use App\Repository\GameRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: GameRepository::class)]
+#[UniqueEntity('name')]
+#[UniqueEntity('slug')]
 class Game
 {
     #[ORM\Id]
