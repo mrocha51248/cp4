7e9b47de5538b061946db20695c2cd96b0e3b0d5
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-02 15:49:43 +0100

    create UserScoreFixtures

---
+++ src/DataFixtures/UserScoreFixtures.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\User;
+use App\Entity\UserScore;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class UserScoreFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        foreach ($this->referenceRepository->getReferences() as $userKey => $userReference) {
+            if (!($userReference instanceof User)) {
+                continue;
+            }
+
+            /** @var User */
+            $user = $this->getReference($userKey);
+
+            foreach ($this->referenceRepository->getReferences() as $categoryKey => $categoryReference) {
+                if (!($categoryReference instanceof Category)) {
+                    continue;
+                }
+    
+                /** @var Category */
+                $category = $this->getReference($categoryKey);
+
+                $elo = 1000 + (500 * (crc32($userKey . $categoryKey) / 0xFFFFFFFF) * 2);
+                $score = (new UserScore())
+                    ->setElo($elo)
+                ;
+                $user->addScore($score);
+                $category->addUserScore($score);
+
+                $manager->persist($score);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            CategoryFixtures::class,
+            UserFixtures::class,
+        ];
+    }
+}
