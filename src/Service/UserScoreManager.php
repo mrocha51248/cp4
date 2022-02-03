decc52739062b0d0c266cc92983ac25490ae3107
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-03 15:24:50 +0100

    Service: create UserScoreManager

---
+++ src/Service/UserScoreManager.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Category;
+use App\Entity\User;
+use App\Entity\UserScore;
+use App\Repository\UserScoreRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserScoreManager
+{
+    public const DEFAULT_ELO = 1500;
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserScoreRepository $userScoreRepository
+    ) {
+    }
+
+    public function getScore(User $user, Category $category): int
+    {
+        $userScore = $this->userScoreRepository->findOneBy(['user' => $user, 'category' => $category]);
+        if (!$userScore) {
+            return self::DEFAULT_ELO;
+        }
+        return $userScore->getElo();
+    }
+
+    public function saveScore(User $user, Category $category, int $elo): int
+    {
+        $userScore = $this->userScoreRepository->findOneBy(['user' => $user, 'category' => $category]);
+
+        if (!$userScore) {
+            $previousElo = self::DEFAULT_ELO;
+            $userScore = new UserScore();
+            $user->addScore($userScore);
+            $category->addUserScore($userScore);
+            $this->entityManager->persist($userScore);
+        } else {
+            $previousElo = $userScore->getElo();
+        }
+
+        $userScore->setElo($elo);
+        $this->entityManager->flush();
+
+        return $previousElo;
+    }
+}
