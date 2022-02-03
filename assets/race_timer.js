13f781f6a6976b490ddc8008b6c804b6613f237a
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-03 15:52:40 +0100

    implement race timer

---
+++ assets/race_timer.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('.race-timer').forEach(function (element) {
+    const startDate = new Date(element.dataset.startTime * 1000).getTime();
+
+    const updateInterval = setInterval(function () {
+        const now = new Date().getTime();
+
+        const negative = startDate > now;
+        const diff = Math.abs(now - startDate);
+
+        const hours = Math.floor(diff / (1000 * 60 * 60));
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+        const milliseconds = Math.floor(diff % 1000);
+
+        let output = '';
+        if (hours > 0) output += hours.toString() + ':';
+        if (!negative || minutes > 0 || hours > 0) output += minutes.toString().padStart(2, '0') + ':';
+        output += seconds.toString().padStart(2, '0') + '.';
+        output += milliseconds.toString().padStart(3, '0');
+
+        element.innerHTML = negative ? '<span class="text-danger">' + output + '</span>' : output;
+    }, 10);
+
+    document.querySelectorAll('.race-timer-stopper').forEach(function (stopper) {
+        stopper.addEventListener('click', function (event) {
+            clearInterval(updateInterval);
+        });
+    });
+});
