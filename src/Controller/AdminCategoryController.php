70f61e4543af0aefed1061960039931288e961cd
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-03 22:34:43 +0100

    create link to admin dashboard

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -6,12 +6,14 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin/category')]
+#[IsGranted('ROLE_ADMIN')]
 class AdminCategoryController extends AbstractController
 {
     #[Route('/', name: 'admin_category_index', methods: ['GET'])]
--- src/Controller/AdminGameController.php
+++ src/Controller/AdminGameController.php
@@ -6,12 +6,14 @@ use App\Entity\Game;
 use App\Form\GameType;
 use App\Repository\GameRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin/game')]
+#[IsGranted('ROLE_ADMIN')]
 class AdminGameController extends AbstractController
 {
     #[Route('/', name: 'admin_game_index', methods: ['GET'])]
