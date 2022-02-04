b94cd4bd7065b6e3411b00b0d616118f931af469
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-04 02:24:24 +0100

    RaceManager: implement closeRace method

--- src/Service/RaceManager.php
+++ src/Service/RaceManager.php
@@ -11,6 +11,8 @@ use App\Repository\RaceResultRepository;
 use DateInterval;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
+use EloRating\Player as EloPlayer;
+use EloRating\Game as EloGame;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class RaceManager
@@ -19,7 +21,8 @@ class RaceManager
         private EntityManagerInterface $entityManager,
         private RaceResultRepository $raceResultRepository,
         private RaceRepository $raceRepository,
-        private UserScoreManager $userScoreManager
+        private UserScoreManager $userScoreManager,
+        private DateIntervalConverter $dateIntervalConverter
     ) {
     }
 
@@ -35,6 +38,96 @@ class RaceManager
         $this->entityManager->flush();
 
         return $race;
+    }
+
+    public function closeRace(Race $race): void
+    {
+        if ($race->getFinishedAt()) {
+            return;
+        }
+
+        $race->setFinishedAt(new DateTimeImmutable());
+
+        $results = $race->getResults();
+        foreach ($results as $result) {
+            if (!$result->getFinishedAt()) {
+                $result->setFinishedAt($race->getCreatedAt());
+            }
+        }
+
+        list($min, $max) = $this->getScoringRange($race);
+        if ($min === null || $max === null) {
+            foreach ($results as $result) {
+                $result->setFinishElo($result->getStartElo());
+            }
+
+            $this->entityManager->flush();
+            return;
+        }
+
+        $times = $this->getRaceTimes($race);
+        foreach ($times as list('time' => $timeMs, 'result' => $result)) {
+            $elo = $result->getStartElo();
+            foreach ($times as list('time' => $otherTimeMs, 'result' => $otherResult)) {
+                if ($otherResult === $result) {
+                    continue;
+                }
+
+                $player1 = new EloPlayer($result->getStartElo());
+                $player2 = new EloPlayer($otherResult->getStartElo());
+
+                $game = new EloGame($player1, $player2);
+                // TODO: Factor in scoring range
+                $game->setScore(
+                        $timeMs !== null && ($timeMs <= $otherTimeMs || $otherTimeMs === null) ? 1 : 0,
+                        $otherTimeMs !== null && ($otherTimeMs <= $timeMs || $timeMs === null) ? 1 : 0
+                    )
+                    ->setK(32)
+                    ->count();
+
+                $elo += $game->getPlayer1()->getRating() - $result->getStartElo();
+            }
+            $result->setFinishElo($elo);
+            $this->userScoreManager->saveScore($result->getUser(), $result->getRace()->getCategory(), $elo);
+        }
+
+        $this->entityManager->flush();
+    }
+
+    public function getRaceTimes(Race $race): array
+    {
+        $times = [];
+        foreach ($race->getResults() as $result) {
+            if (!$result->isFinished() || $result->isForfeited()) {
+                $times[] = ['time' => null, 'result' => $result];
+                continue;
+            }
+            $timeMs = $this->dateIntervalConverter->getMillisecondsTotal(
+                $result->getStartedAt()->diff($result->getFinishedAt())
+            );
+            $times[] = ['time' => $timeMs, 'result' => $result];
+        }
+        return $times;
+    }
+
+    public function getScoringRange(Race $race): array
+    {
+        list($min, $max) = [null, null];
+        foreach ($race->getResults() as $result) {
+            if (!$result->isFinished() || $result->isForfeited()) {
+                continue;
+            }
+            $timeMs = $this->dateIntervalConverter->getMillisecondsTotal(
+                $result->getStartedAt()->diff($result->getFinishedAt())
+            );
+            if ($timeMs > $max || $max === null) {
+                $max = $timeMs;
+            }
+            if ($timeMs < $min || $min === null) {
+                $min = $timeMs;
+            }
+        }
+        return [$min, $max];
     }
 
     public function getRaceToJoin(User $user, Category $category): ?Race
