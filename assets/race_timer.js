6dc2ad951cb0a358bb01288f1ba6d717b2d549d9
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-09 14:46:08 +0100

    race time: round to the hundredths

--- assets/race_timer.js
+++ assets/race_timer.js
@@ -22,7 +22,7 @@ document.querySelectorAll('.race-timer').forEach(function (element) {
         if (hours > 0) output += hours.toString() + ':';
         if (!negative || minutes > 0 || hours > 0) output += minutes.toString().padStart(2, '0') + ':';
         output += seconds.toString().padStart(2, '0') + '.';
-        output += milliseconds.toString().padStart(3, '0');
+        output += Math.floor(milliseconds / 10).toString().padStart(2, '0');
 
         element.innerHTML = negative ? '<span class="text-danger">' + output + '</span>' : output;
     }, 10);
